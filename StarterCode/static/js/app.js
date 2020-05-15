a6a85033683359eee4956fac7e9767eedc1f8b29
Terry Priest <jeepyklp@yahoo.com>
2020-05-15 10:45:34 -0700

    adding code

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,27 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
+var button = d3.select("#filter-btn")
 
-// YOUR CODE HERE!
+
+// BONUS: Refactor to use Arrow Functions!
+button.on("click", function(){
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    tbody.html("");
+    data.forEach((tableData) => {
+        var row = tbody.append("tr");
+        // console.log(tableData.datetime);
+        if (tableData.datetime == inputValue) 
+        {Object.entries(tableData).forEach(([key, value]) => {
+          
+          var cell = row.append("td");
+          cell.text(value);}
+        )};
+      });
+
+});
+
+
+
+  
